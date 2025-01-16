81d149d0ca39d7c06bcf924ac1ec0d347ac36eb0
btwonion <btwonion@gmx.de>
2025-01-16 18:26:01 +0100

    correctly name function

--- paper/src/main/kotlin/dev/nyon/magnetic/extensions/MagneticCheck.kt
+++ paper/src/main/kotlin/dev/nyon/magnetic/extensions/MagneticCheck.kt
@@ -5,13 +5,13 @@ import dev.nyon.magnetic.magneticKey
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
-fun ItemStack.hasTelekinesis(): Boolean = hasItemMeta() && itemMeta.enchants.any { it.key.key == magneticKey }
+fun ItemStack.hasMagnetic(): Boolean = hasItemMeta() && itemMeta.enchants.any { it.key.key == magneticKey }
 
 fun Player.isEligible(): Boolean {
     val enchantmentValid = !config.needEnchantment || config.needEnchantment && listOf(
         inventory.itemInMainHand,
         inventory.itemInMainHand
-    ).any { it.hasTelekinesis() }
+    ).any { it.hasMagnetic() }
     val crouchValid = !config.needSneak || config.needSneak && isSneaking
     return enchantmentValid && crouchValid
 }
