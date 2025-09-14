6311244c2b0a1c0b1ddfed6f3c8421f50e0ecb59
Anton Wiblishauser <btwonion@gmx.de>
2025-09-14 16:27:49 +0200

    implement alerts on Paper as well

--- paper/src/main/kotlin/dev/nyon/magnetic/config/Config.kt
+++ paper/src/main/kotlin/dev/nyon/magnetic/config/Config.kt
@@ -1,10 +1,16 @@
 package dev.nyon.magnetic.config
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.boolean
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.entity.Player
 
 lateinit var config: Config
 
@@ -16,8 +22,45 @@ data class Config(
     var itemsAllowed: Boolean = true,
     var expAllowed: Boolean = true,
     var ignoreRangedWeapons: Boolean = true,
-    var ignoreEntities: List<Identifier> = listOf()
-)
+    var ignoreEntities: List<Identifier> = listOf(),
+    var fullInventoryAlert: FullInventoryAlert = FullInventoryAlert()
+) {
+    @Serializable
+    data class FullInventoryAlert(
+        var soundAlert: SoundAlert = SoundAlert(), var textAlert: TextAlert = TextAlert()
+    ) {
+        interface Alert {
+            var enabled: Boolean
+            var cooldownInSeconds: Int
+
+            fun invoke(player: Player)
+        }
+
+        @Serializable
+        data class SoundAlert(
+            override var enabled: Boolean = true, override var cooldownInSeconds: Int = 5
+        ) : Alert {
+            @Transient
+            private val sound = Sound.sound(Key.key("block.note_block.pling"), Sound.Source.MASTER, 1f, 1f)
+
+            override fun invoke(player: Player) {
+                player.playSound(sound)
+            }
+        }
+
+        @Serializable
+        data class TextAlert(
+            override var enabled: Boolean = true, override var cooldownInSeconds: Int = 60
+        ) : Alert {
+            override fun invoke(player: Player) {
+                player.sendMessage(
+                    Component.text("[magnetic] Your inventory is full!")
+                        .color(NamedTextColor.GOLD)
+                )
+            }
+        }
+    }
+}
 
 internal fun migrate(jsonElement: JsonElement, version: Int?): Config? {
     val jsonObject = jsonElement.jsonObject
