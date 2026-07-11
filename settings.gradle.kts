49817eaf1063ff60398be3867ab7eb1a0a5d75d5
btwonion <79146501+btwonion@users.noreply.github.com>
2026-07-11 23:28:58 +0200

    build: add minecraft 26.1.2 targets

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,8 @@ stonecutter {
     kotlinController = true
     centralScript = "build.gradle.kts"
     shared {
+        version("26.1.2-fabric", "26.1.2")
+        version("26.1.2-neoforge", "26.1.2")
         version("26.2-fabric", "26.2-pre-4")
         version("26.2-neoforge", "26.2")
         vcsVersion = "26.2-fabric"
