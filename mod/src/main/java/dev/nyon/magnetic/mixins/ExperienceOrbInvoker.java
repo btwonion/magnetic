7bcded3ab2251aa4976a40b84e460f1fc5547c8b
Anton Wiblishauser <79146501+btwonion@users.noreply.github.com>
2026-07-16 20:36:06 +0200

    fix: adapt experience orb accessor for 1.21.1

--- mod/src/main/java/dev/nyon/magnetic/mixins/ExperienceOrbInvoker.java
+++ mod/src/main/java/dev/nyon/magnetic/mixins/ExperienceOrbInvoker.java
@@ -3,6 +3,9 @@ package dev.nyon.magnetic.mixins;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.ExperienceOrb;
 import org.spongepowered.asm.mixin.Mixin;
+/*? if <1.21.11 {*/
+/*import org.spongepowered.asm.mixin.gen.Accessor;
+*//*?}*/
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ExperienceOrb.class)
@@ -17,6 +20,10 @@ public interface ExperienceOrbInvoker {
     @Invoker("getValue")
     int invokeGetValue();
 
+    /*? if >=1.21.11 {*/
     @Invoker("setValue")
+    /*?} else {*/
+    /*@Accessor("value")
+    *//*?}*/
     void invokeSetValue(int value);
 }
