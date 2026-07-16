63ad6180853f9d9f0963194f10ac08139a0e042e
Anton Wiblishauser <79146501+btwonion@users.noreply.github.com>
2026-07-16 19:46:58 +0200

    fix: correct 1.21.1 bucket mixin descriptors

--- mod/src/main/java/dev/nyon/magnetic/mixins/fluids/BucketItemMixin.java
+++ mod/src/main/java/dev/nyon/magnetic/mixins/fluids/BucketItemMixin.java
@@ -6,7 +6,11 @@ import dev.nyon.magnetic.utils.PositionTracker;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+/*? if >=1.21.11 {*/
 import net.minecraft.world.entity.LivingEntity;
+/*?} else {*/
+/*import net.minecraft.world.entity.player.Player;
+*//*?}*/
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -20,15 +24,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class BucketItemMixin {
 
     @Inject(
-        method =  /*? if fabric {*/ "emptyContents" /*?} else {*/ /*"emptyContents(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;Lnet/minecraft/world/item/ItemStack;)Z" *//*?}*/,
+        method =  /*? if fabric {*/ "emptyContents" /*?} else if >=1.21.11 {*/ /*"emptyContents(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;Lnet/minecraft/world/item/ItemStack;)Z" *//*?} else {*/ /*"emptyContents(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/BlockHitResult;Lnet/minecraft/world/item/ItemStack;)Z" *//*?}*/,
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/item/BucketItem;playEmptySound(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;)V",
+            target = /*? if >=1.21.11 {*/ "Lnet/minecraft/world/item/BucketItem;playEmptySound(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;)V" /*?} else {*/ /*"Lnet/minecraft/world/item/BucketItem;playEmptySound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;)V" *//*?}*/,
             shift = At.Shift.AFTER
         )
     )
     private void recordBucketPlacement(
-        LivingEntity user,
+        /*? if >=1.21.11 {*/ LivingEntity /*?} else {*/ /*Player *//*?}*/ user,
         Level level,
         BlockPos pos,
         BlockHitResult hitResult,
